7c40f3e7e706321a44bf582dcc3fd6a9958a5a62
Windi94 <Marcel.Windischmann@gmail.com>
2025-07-02 19:09:55 +0200

    Initial Commit

---
+++ icon_layer/examples/IconLayers/providers.js
@@ -0,0 +1,30 @@
+(function(factory) {
+    if (typeof module !== 'undefined' && module.exports) {
+        module.exports = factory(require('leaflet'));
+    } else {
+        window.providers = factory(window.L);
+    }
+})(function(L) {
+    var providers = {};
+
+    providers['Mundialis'] = {
+        title: 'Mundialis',
+        icon: 'icons/MundialisIcon.jpg',
+        layer: L.tileLayer.wms('https://ows.mundialis.de/services/service?', {
+            layers: 'TOPO-WMS',
+			maxZoom: 18,
+            attribution: '&copy; Mundialis'
+        })
+    };
+
+    providers['Terrestris'] = {
+        title: 'Terrestris',
+		icon: 'icons/TerrestrisIcon.jpg',
+        layer: L.tileLayer.wms('https://ows.terrestris.de/osm/service?', {
+            layers: 'OSM-WMS',maxZoom: 18,
+            attribution: '&copy; Terrestris'
+        })
+    }
+
+    return providers;
+});
