1bf8666961c6bc3b788a01e60345afd1b1c85fc6
Windi94 <Marcel.Windischmann@gmail.com>
2025-07-02 19:54:34 +0200

    Update providers.js

--- icon_layer/examples/IconLayers/providers.js
+++ icon_layer/examples/IconLayers/providers.js
@@ -21,7 +21,8 @@
         title: 'Terrestris',
 		icon: 'icons/TerrestrisIcon.jpg',
         layer: L.tileLayer.wms('https://ows.terrestris.de/osm/service?', {
-            layers: 'OSM-WMS',maxZoom: 18,
+            layers: 'OSM-WMS',
+			maxZoom: 18,
             attribution: '&copy; Terrestris'
         })
     }
